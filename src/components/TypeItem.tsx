053de01aa014135f4b2a7c8859b8a7e42d75e573
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-07-19 15:49:38 +0200

    Added documentation for TypeItem.tsx

--- src/components/TypeItem.tsx
+++ src/components/TypeItem.tsx
@@ -1,13 +1,32 @@
-import { cellColor } from "@/helpers/cellColor";
-import { gridStore } from "@/store/gridStore";
-import { Button } from "./common/Button";
+import { VNode } from "preact";
 
+import { Button } from "./common/Button";
+import { gridStore } from "@/store/gridStore";
+import { cellColor } from "@/helpers/cellColor";
+
+/**
+ * Props for the ToolbarItem component.
+ *
+ * @typedef {Object} Props
+ * @property {cellColor} color - The color of the toolbar item.
+ * @property {string} name - The name of the toolbar item.
+ */
 type Props = {
   color: cellColor;
   name: string;
 };
 
-export function ToolbarItem({ color, name }: Props) {
+/**
+ * ToolbarItem component.
+ *
+ * @param {Props} props - The props for the ToolbarItem component.
+ * @returns {JSX.Element} The rendered ToolbarItem component.
+ */
+export function ToolbarItem({ color, name }: Props): VNode {
+  /**
+   * Sets the selected color in the grid store.
+   * @returns {void}
+   */
   function setSelectedColor() {
     gridStore.selectedColor = color;
   }
