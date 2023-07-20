d349bfc186c780ffb5495eb451246e5793006eaa
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-07-20 13:46:08 +0200

    Further optimizations

--- src/lib/loadTemplate.ts
+++ src/lib/loadTemplate.ts
@@ -12,38 +12,37 @@ type TemplateInfo = {
 export function loadTemplate(t: TemplateInfo): void {
   resizeGrid(t.size);
 
-  let x = parseInt(t.start.split(":")[0]);
-  let y = parseInt(t.start.split(":")[1]);
-  gridStore.grid.setCellValue(x, y, CellValue.Start);
+  const { start, end, walls } = t;
+  const [startX, startY] = start.split(":").map(coord => parseInt(coord));
+  const [endX, endY] = end.split(":").map(coord => parseInt(coord));
 
-  x = parseInt(t.end.split(":")[0]);
-  y = parseInt(t.end.split(":")[1]);
-  gridStore.grid.setCellValue(x, y, CellValue.End);
+  gridStore.grid.setCellValue(startX, startY, CellValue.Start);
+  gridStore.grid.setCellValue(endX, endY, CellValue.End);
 
-  for (const wallCoord of t.walls) {
-    const [xCoords, yCoords] = wallCoord.split(":");
-    setWallCoordinates(xCoords, yCoords);
-  }
+  walls.forEach(setWallCoordinates);
 }
 
-function setWallCoordinates(xCoords: string, yCoords: string): void {
-  const parseRange = (range: string): { start: number; end: number } => {
-    const rangeParts = range.split("-");
-    const start = parseInt(rangeParts[0]);
-    const end = rangeParts.length > 1 ? parseInt(rangeParts[1]) : start;
-    return { start, end };
-  };
+function parseRange(range: string): { start: number; end: number } {
+  const [start, end] = range.split("-").map(Number);
+  return { start, end: end || start };
+}
 
+function setWallCoordinates(wallCoord: string): void {
+  const [xCoords, yCoords] = wallCoord.split(":");
   const xRanges = xCoords.split(",").map(parseRange);
   const yRanges = yCoords.split(",").map(parseRange);
 
-  for (const { start: startXStart, end: startXEnd } of xRanges) {
-    for (const { start: startYStart, end: startYEnd } of yRanges) {
-      for (let y = startYStart; y <= startYEnd; y++) {
-        for (let x = startXStart; x <= startXEnd; x++) {
-          gridStore.grid.setCellValue(y, x, CellValue.Wall);
-        }
-      }
+  for (const { start: startYStart, end: startYEnd } of yRanges) {
+    for (const { start: startXStart, end: startXEnd } of xRanges) {
+      setWallCells(startXStart, startXEnd, startYStart, startYEnd);
+    }
+  }
+}
+
+function setWallCells(startX: number, endX: number, startY: number, endY: number): void {
+  for (let y = startY; y <= endY; y++) {
+    for (let x = startX; x <= endX; x++) {
+      gridStore.grid.setCellValue(y, x, CellValue.Wall);
     }
   }
 }
