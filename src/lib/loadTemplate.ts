46c302a8f1a99218c1ac99582d9d63b862224c83
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-07-20 12:30:03 +0200

    Simplified template parsing

--- src/lib/loadTemplate.ts
+++ src/lib/loadTemplate.ts
@@ -12,14 +12,31 @@ type TemplateInfo = {
 export function loadTemplate(t: TemplateInfo): void {
   resizeGrid(t.size);
 
-  let [x, y] = t.start.split(",");
-  gridStore.grid.setCellValue(parseInt(x), parseInt(y), CellValue.Start);
-  [x, y] = t.end.split(",");
-  gridStore.grid.setCellValue(parseInt(x), parseInt(y), CellValue.End);
+  const [startX, startY] = t.start.split(":");
+  gridStore.grid.setCellValue(parseInt(startX), parseInt(startY), CellValue.Start);
+
+  const [endX, endY] = t.end.split(":");
+  gridStore.grid.setCellValue(parseInt(endX), parseInt(endY), CellValue.End);
 
   for (let i = 0; i < t.walls.length; i++) {
-    const [x, y] = t.walls[i].split(",");
+    const wallCoordinates = t.walls[i].match(/(\d+)-?(\d*):(\d+)-?(\d*)/);
 
-    gridStore.grid.setCellValue(parseInt(y), parseInt(x), CellValue.Wall);
+    if (wallCoordinates) {
+      const [, startXRange, endXRange, startYRange, endYRange] = wallCoordinates;
+      const startXStart = parseInt(startXRange);
+      const startXEnd = endXRange ? parseInt(endXRange) : startXStart;
+      const startYStart = parseInt(startYRange);
+      const startYEnd = endYRange ? parseInt(endYRange) : startYStart;
+
+      for (let y = startYStart; y <= startYEnd; y++) {
+        for (let x = startXStart; x <= startXEnd; x++) {
+          gridStore.grid.setCellValue(y, x, CellValue.Wall);
+        }
+      }
+    } else {
+      // Handle single coordinate
+      const [x, y] = t.walls[i].split(":");
+      gridStore.grid.setCellValue(parseInt(y), parseInt(x), CellValue.Wall);
+    }
   }
 }
