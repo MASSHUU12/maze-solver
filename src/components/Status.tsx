12f221c582291a33a89ec4e63149c8cffe500b56
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-07-20 10:34:49 +0200

    Added documentation for Status.tsx

--- src/components/Status.tsx
+++ src/components/Status.tsx
@@ -1,11 +1,22 @@
-import { statusStore } from "@/store/statusStore";
 import { VNode } from "preact";
-import { useEffect } from "preact/hooks";
 import { useSnapshot } from "valtio";
+import { useEffect } from "preact/hooks";
 
+import { statusStore } from "@/store/statusStore";
+
+/**
+ * Component to display the status message.
+ *
+ * @returns {VNode} The status component.
+ */
 export function Status(): VNode {
   const store = useSnapshot(statusStore);
 
+  /**
+   * Set the initial status message on component mount.
+   *
+   * @returns {void}
+   */
   useEffect(() => {
     statusStore.status =
       'Press "Solve" to solve the maze, "Clean" to remove the solution, and "Reset" to reset the board.';
