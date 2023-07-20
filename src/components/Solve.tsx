a83d7b70ba140bd9eae1a4dfcb10012298bfa28a
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-07-20 10:20:37 +0200

    Added two status messages

--- src/components/Solve.tsx
+++ src/components/Solve.tsx
@@ -6,6 +6,7 @@ import { convertGridToGraph } from "@/lib/graph";
 import { Button } from "./common/Button";
 import { gridStore } from "@/store/gridStore";
 import { findStartNode } from "@/helpers/findStartNode";
+import { statusStore } from "@/store/statusStore";
 
 /**
  * Component for solving a grid.
@@ -18,11 +19,19 @@ export function Solve(): VNode {
     const graph = convertGridToGraph(gridStore.grid);
     const start = findStartNode(graph);
 
-    if (start === null) return;
+    if (start === null) {
+      statusStore.status = "Maze does not have a start, add it and try again.";
+      return;
+    }
 
     const path = bfs(start);
 
-    path?.forEach(node => {
+    if (path === null) {
+      statusStore.status = "The maze has no solution.";
+      return;
+    }
+
+    path.forEach(node => {
       gridStore.grid.setCellValue(node.row, node.col, node.value);
     });
   }
