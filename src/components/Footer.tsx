7960f90a820b3982695f8819ea3bc481732d27f3
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-07-21 14:04:00 +0200

    Finished Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,19 @@
-export function Footer() {
+import { VNode } from "preact";
+import { Link } from "./common/Link";
+
+/**
+ * Represents the footer component.
+ *
+ * @returns {VNode} The virtual DOM representation of the footer component.
+ */
+export function Footer(): VNode {
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer class="p-3 bg-slate-200 flex flex-col">
+      <Link link="https://github.com/MASSHUU12/maze-solver">Source code</Link>
+      <Link link="https://masshuu12.github.io/">Portfolio</Link>
+      <p>
+        Licensed under <Link link="https://github.com/MASSHUU12/maze-solver/blob/master/LICENSE">MIT license</Link>.
+      </p>
     </footer>
   );
 }
