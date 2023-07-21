76d0e2f56fc256faf0e63fc728a2c26b1740b14b
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2023-07-21 14:05:10 +0200

    Added documentation for Link component

--- src/components/common/Link.tsx
+++ src/components/common/Link.tsx
@@ -5,6 +5,12 @@ type Props = {
   children: ComponentChildren;
 };
 
+/**
+ * Link component renders an anchor element with the specified URL.
+ *
+ * @param {Props} props - The props object for the Link component.
+ * @returns {VNode} A virtual DOM node representing the anchor element.
+ */
 export function Link({ link, children }: Props): VNode {
   return (
     <a href={link} target="_blank" rel="noreferrer" class="underline">
